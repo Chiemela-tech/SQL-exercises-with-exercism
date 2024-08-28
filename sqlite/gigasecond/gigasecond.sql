641f72c019c28550cf6f8f29e3fa71a68e65c865
Chiemela Chinedum <cchiemela2@gmail.com>
2024-08-28 12:27:01 +0100

    updated sql

--- sqlite/gigasecond/gigasecond.sql
+++ sqlite/gigasecond/gigasecond.sql
@@ -1,2 +1,16 @@
 -- Schema: CREATE TABLE "gigasecond" ("moment" TEXT, "result" TEXT);
 -- Task: update the gigasecond table and set the result based on the moment.
+CREATE TABLE "gigasecond" (
+    "moment" TEXT,
+    "result" TEXT
+);
+
+-- Insert a sample date
+INSERT INTO gigasecond (moment) VALUES ('2011-04-25');
+
+-- Update the table to calculate the gigasecond anniversary
+UPDATE gigasecond
+SET result = replace(datetime(moment, '+1000000000 seconds'), ' ', 'T');
+
+-- Query to see the result
+SELECT * FROM gigasecond;
--- sqlite/grains/grains.sql
+++ sqlite/grains/grains.sql
@@ -1,2 +1,18 @@
 -- Schema: CREATE TABLE "grains" ("task" TEXT, "square" INT, "result" INT);
 -- Task: update the grains table and set the result based on the task (and square fields).
+CREATE TABLE grains (
+    task TEXT,
+    square INT,
+    result INT
+);
+
+UPDATE grains
+SET result = CASE 
+    WHEN task = 'single-square' THEN POW(2, square - 1)
+    WHEN task = 'total' THEN POW(2, 64) - 1
+END;
+-- Get the number of grains on a specific square (e.g., square 1)
+SELECT * FROM grains WHERE task = 'single-square' AND square = 1;
+
+-- Get the total number of grains on the chessboard
+SELECT * FROM grains WHERE task = 'total';
