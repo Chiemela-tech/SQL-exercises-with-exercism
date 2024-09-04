fb63b7d5f38a75923f02f0fc3d7e98081335611d
Chiemela Chinedum <cchiemela2@gmail.com>
2024-09-04 15:38:48 +0100

    updated

--- sqlite/leap/leap.sql
+++ sqlite/leap/leap.sql
@@ -1,2 +1,12 @@
 -- Schema: CREATE TABLE "leap" ( "year" INT, "is_leap" BOOL);
 -- Task: update the leap table and set the is_leap based on the year field.
+CREATE TABLE leap(
+    year INT,
+    is_leap BOOL
+)
+
+UPDATE leap
+SET is_leap = CASE
+    WHEN year % 4 = 0 AND (year % 100 != 0 OR year % 400 = 0) THEN TRUE
+    ELSE FALSE
+END;
