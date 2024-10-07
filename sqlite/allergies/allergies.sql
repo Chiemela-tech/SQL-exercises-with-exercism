c81363a7b0507dad55df88fe17b3c89d1197216b
Chiemela Chinedum <cchiemela2@gmail.com>
2024-10-08 00:42:21 +0100

    updated allergies

--- sqlite/allergies/allergies.sql
+++ sqlite/allergies/allergies.sql
@@ -4,42 +4,27 @@
 --       - For the `list` task you have to write corresponding items to the result field
 
 -- Allergens and their corresponding scores
-WITH allergens AS (
-    SELECT 'eggs' AS item, 1 AS score UNION
-    SELECT 'peanuts', 2 UNION
-    SELECT 'shellfish', 4 UNION
-    SELECT 'strawberries', 8 UNION
-    SELECT 'tomatoes', 16 UNION
-    SELECT 'chocolate', 32 UNION
-    SELECT 'pollen', 64 UNION
-    SELECT 'cats', 128
-)
-
--- Update based on the task
+CREATE TABLE allergens(item TEXT, code INTEGER);
+INSERT INTO allergens (item, code) VALUES
+    ('eggs', 1),
+    ('peanuts', 2),
+    ('shellfish', 4),
+    ('strawberries', 8),
+    ('tomatoes', 16),
+    ('chocolate', 32),
+    ('pollen', 64),
+    ('cats', 128);
 UPDATE allergies
-SET result = CASE 
-    -- Check if allergic to a specific item (allergicTo task)
-    WHEN task = 'allergicTo' THEN 
-        CASE 
-            -- Perform bitwise AND to check if the item is in the score
-            WHEN (score & (SELECT score FROM allergens WHERE item = allergies.item)) > 0 THEN 'true'
-            ELSE 'false'
-        END
-
-    -- List all allergens (list task)
-    WHEN task = 'list' THEN (
-        SELECT GROUP_CONCAT(item, ', ') 
-        FROM allergens 
-        WHERE (score & allergens.score) > 0
-    )
-END
-WHERE task IN ('allergicTo', 'list');
-
-    -- List all allergens (list task)
-    WHEN task = 'list' THEN (
-        SELECT STRING_AGG(item, ', ') 
-        FROM allergens 
-        WHERE (score & allergens.score) > 0
-    )
-END
-WHERE task IN ('allergicTo', 'list');
+SET result = (
+            SELECT CASE WHEN allergies.score & code > 0 THEN 'true' ELSE 'false' END
+            FROM  allergens
+            WHERE allergies.item == item
+        )
+WHERE task == 'allergicTo';
+UPDATE allergies
+SET result = (
+            SELECT CASE score WHEN 0 THEN '' ELSE group_concat(item, ', ') END
+            FROM allergens
+            WHERE allergies.score & code > 0
+        )
+WHERE task == 'list';
