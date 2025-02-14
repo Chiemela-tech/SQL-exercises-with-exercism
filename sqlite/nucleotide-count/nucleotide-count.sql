a9361e88d7a9e059be59f1d184286fda278275b3
Chiemela Chinedum <cchiemela2@gmail.com>
2025-02-14 13:54:20 +0000

    updated

--- sqlite/nucleotide-count/nucleotide-count.sql
+++ sqlite/nucleotide-count/nucleotide-count.sql
@@ -4,7 +4,8 @@
 DROP TABLE IF EXISTS "nucleotide-count";
 CREATE TABLE "nucleotide-count" (
     "strand" TEXT,
-    "result" TEXT
+    "result" TEXT,
+    CHECK (strand REGEXP '^[ACGT]*$')
 );
 
 -- Please don't change the following two import lines. Feel free to edit the previous lines, though.
@@ -12,3 +13,19 @@ CREATE TABLE "nucleotide-count" (
 .import ./data.csv "nucleotide-count"
 
 -- Write your code below. Feel free to edit the CREATE TABLE above, too!
+WITH nucleotides(base) AS (
+    VALUES ('A'), ('C'), ('G'), ('T')
+)
+UPDATE "nucleotide-count"
+SET result = (
+    SELECT json_object(
+        'A', sum(CASE WHEN base = 'A' THEN c ELSE 0 END),
+        'C', sum(CASE WHEN base = 'C' THEN c ELSE 0 END),
+        'G', sum(CASE WHEN base = 'G' THEN c ELSE 0 END),
+        'T', sum(CASE WHEN base = 'T' THEN c ELSE 0 END)
+    )
+    FROM (
+        SELECT base, LENGTH("nucleotide-count".strand) - LENGTH(REPLACE("nucleotide-count".strand, base, '')) AS c
+        FROM nucleotides
+    )
+);
